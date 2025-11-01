70c9131ff266e51775f051441cdc0794c887e3d5
Luis Mendez <slpmendez60@gmail.com>
2025-10-31 23:04:50 -0500

    feat: implementación de operadores de asignación e incremento

---
+++ src/IncrementOperator.java
@@ -0,0 +1,15 @@
+public class IncrementOperator {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        // TODO: Aplicar incremento
+        System.out.println("Valor inicial: " + contador);
+
+contador++;
+System.out.println("Después de contador++: " + contador);
+
+++contador;
+System.out.println("Después de ++contador: " + contador);
+
+    }
+}
